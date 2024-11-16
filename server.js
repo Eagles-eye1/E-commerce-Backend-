6060ea4048ec8e920a22ec7cbe85e793139f26b3
Powell Orok <dominicp276@gmail.com>
2024-11-16 11:58:05 +0100

    Update

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const app = express()
+const cors = require('cors');
+app.use(cors());
 
+const names = ["Gold","silver","frankicense" ,"myrrh", "salt"]
+
+app.get("/api/names",(req,res)=>{res.json(names)})
 
 const PORT = process.env.PORT || 3000
 
